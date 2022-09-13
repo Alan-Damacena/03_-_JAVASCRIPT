ff218da9fd4a0cb01ec73dc84dbbd4bdb1bc2dea
Alan Damacena <alanfmdamacena@outlook.com>
2022-09-13 11:41:35 -0300

    js

---
+++ 02_-_INNER_HTML/script.js
@@ -0,0 +1,2 @@
+// Selecionando o elemento HTML pelo ID e imprimindo uma mensagem na tela
+document.getElementById("texto").innerHTML="Utilizando o InnerHTML para imprimir um texto em <b>JS</b>!";
---
+++ 04_-_ALERT/script.js
@@ -0,0 +1,6 @@
+alert("Oi, isso é um alerta !!!!!")
+alert("TESTE")
+alert("1 + 1")
+alert("É IGUAL A")
+alert(1+1)
+
