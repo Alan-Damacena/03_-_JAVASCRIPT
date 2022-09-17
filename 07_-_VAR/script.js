795bdd1295c162d3e5127cbdc6f1c415725e6943
Alan Damacena <alanfmdamacena@outlook.com>
2022-09-17 11:44:58 -0300

    js

---
+++ 07_-_VAR/script.js
@@ -0,0 +1,24 @@
+/* 
+Variáveis 
+
+São "recipientes" onde pode-se armazenar informações que podem variar, ou seja, podem ter qualquer tipo de valor.
+
+No Javascript tem 3 palavras-chaves para declarar variáveis:
+-> var
+-> let
+-> const
+*/
+
+//DECLARAÇÃO DE VARIÁVEIS
+var a,b,c,nome,sobrenome;
+
+//ATRIBUIÇÃO DOS VALORES
+var a=5;
+var b=5;
+var c=a+b;
+var nome="Alan"
+var sobrenome="Damacena"
+
+//CHAMANDO AS VARIÁVEIS
+alert(c)
+alert(nome+" "+sobrenome)
