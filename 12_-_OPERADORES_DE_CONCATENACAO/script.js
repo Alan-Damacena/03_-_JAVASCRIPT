28bb10e763a90e6a7143d1f69aaba850276f9ffe
Alan Damacena <alanfmdamacena@outlook.com>
2022-09-18 11:11:23 -0300

    js

---
+++ 12_-_OPERADORES_DE_CONCATENACAO/script.js
@@ -0,0 +1,26 @@
+/*
+
+Os operadores Javascript são usados para atribuir valores, comparar valores, executar operações aritméticas e muito mais.
+
+São os sinais que usamos: + - * / ++ -- += -= && || etc...
+
+São separados em 6 categorias:
+
+1) Operadores Aritméticos (matemáticos)
+2) Operadores de Atribuição
+3) Operadores de Sequência
+4) Operadores de Comparação
+5) Operador Condicional (Ternário)
+6) Operadores Lógicos
+
+*/
+
+//Os Operadores de Atribuição nada mais é do que atribuir um valor a uma variável por meio do sinal de igual (=)
+
+var valor1, valor2, total;
+
+valor1 = "Alan ";
+valor2 = "Damacena";
+total = valor1 + valor2;
+
+alert(total)
