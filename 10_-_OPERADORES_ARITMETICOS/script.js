43d09ebd20d0c58488e763e708cc1663c1f95ad0
Alan Damacena <alanfmdamacena@outlook.com>
2022-09-18 10:51:51 -0300

    js

---
+++ 10_-_OPERADORES_ARITMETICOS/script.js
@@ -0,0 +1,37 @@
+/*
+
+Os operadores Javascript são usados para atribuir valores, comparar valores, executar operações aritméticas e muito mais.
+
+São os sinais que usamos: + - * / ++ -- += -= && || etc...
+
+São separados em 6 categorias:
+
+1) Operadores Aritméticos (matemáticos)
+2) Operadores de Atribuição
+3) Operadores de Sequência
+4) Operadores de Comparação
+5) Operador Condicional (Ternário)
+6) Operadores Lógicos
+
+*/
+
+var valor1, valor2, total;
+
+valor1 = 5;
+valor2 = 2;
+
+// Exemplo de Operador Aritmético
+Adicao = valor1 + valor2;
+Subtracao = valor1 - valor2;
+Multiplicacao = valor1 * valor2;
+Divisao = valor1 / valor2;
+Decremento = --valor1;
+Incremento = ++valor2;
+
+
+alert("SOMA " + Adicao);
+alert("SUBTRAÇÃO " + Subtracao);
+alert("MULTIPLICAÇÃO " + Multiplicacao);
+alert("DIVISÃO " + Divisao);
+alert("DECREMENTO de 5 = " + Decremento);
+alert("INCREMENTO de 2 = " + Incremento);
