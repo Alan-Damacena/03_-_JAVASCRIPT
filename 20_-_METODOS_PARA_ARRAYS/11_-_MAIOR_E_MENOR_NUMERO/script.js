ac7c337e4e084754e1c853478d01189118c8aef3
Alan Damacena <alanfmdamacena@outlook.com>
2022-10-20 22:40:24 -0300

    js

--- 20_-_METODOS_PARA_ARRAYS/11_-_MAIOR_E_MENOR_NUMERO/script.js
+++ 20_-_METODOS_PARA_ARRAYS/11_-_MAIOR_E_MENOR_NUMERO/script.js
@@ -21,5 +21,12 @@ function MaiorNumero(array){
     return Math.max.apply(null, array);
 }
 
+function MenorNumero(array){
+    return Math.min.apply(null, array);
+}
+
 document.getElementById("teste").innerHTML = MaiorNumero(lista);
+
+
+document.getElementById("teste2").innerHTML = MenorNumero(lista);
 
