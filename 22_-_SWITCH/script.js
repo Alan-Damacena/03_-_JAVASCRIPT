139fd76394e5e249efeb3c0a343e19ba26ef7e0a
Alan Damacena <alanfmdamacena@outlook.com>
2022-10-23 13:46:49 -0300

    js

---
+++ 22_-_SWITCH/script.js
@@ -0,0 +1,32 @@
+/* SWITCH
+
+É usado ara realizar diferentes ações com base em diferentes condições no mesmo bloco de verificação. Caso a condição não seja compatível nao será executada e o valor padrão será acionado. */
+
+function verificarCor(){
+    const cor = document.getElementById("cor").value;
+
+    switch (cor) {
+
+        case "azul":
+            //o que acontece
+            document.body.style.backgroundColor = "blue"
+            break;
+
+        case "vermelho":
+            //o que acontece
+            document.body.style.backgroundColor = "red"
+            break;
+
+        case "amarelo":
+            //o que acontece
+            document.body.style.backgroundColor = "yellow"
+            break;
+
+        default:
+        //o que acontece
+        document.body.style.backgroundColor = "white"
+        document.getElementById("teste").innerHTML = "Nenhuma cor disponível para " + cor;
+    }
+
+}
+
