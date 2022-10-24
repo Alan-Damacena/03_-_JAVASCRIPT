2919d9301ae7c7845cb0499cabec4fbfd7e4ee6a
Alan Damacena <alanfmdamacena@outlook.com>
2022-10-23 21:07:41 -0300

    js

--- 08_-_LET/script.js
+++ 08_-_LET/script.js
@@ -1,20 +1,13 @@
-/* 
-Variáveis 
+/* VARIÁVEIS
 
-São "recipientes" onde pode-se armazenar informações que podem variar, ou seja, podem ter qualquer tipo de valor.
-
-No Javascript tem 3 palavras-chaves para declarar variáveis:
--> var
--> let
--> const
-*/
+São "recipientes" onde pode-se armazenar informações que podem variar, ou seja, podem ter qualquer tipo de valor. No Javascript tem 3 palavras-chaves para declarar variáveis: -> var -> let -> const */
 
 //ATRIBUIÇÃO DAS VARIÁVEIS
 let a=5;
 let b=5;
 let c=a+b;
-let nome = "Alan";
-let sobrenome = "Damacena";
+let nome = "TESTANDO ";
+let sobrenome = "A VARIÁVEL LET";
 let NomeCompleto = nome + " " + sobrenome;
 
 //CHAMANDO AS VARIÁVEIS
