df19b6cc3490bdba71943686435038b84366e269
Alan Damacena <alanfmdamacena@outlook.com>
2022-10-27 21:22:49 -0300

    js

--- 23_-_LACO_DE_REPETICAO_FOR/script.js
+++ 23_-_LACO_DE_REPETICAO_FOR/script.js
@@ -1,6 +1,10 @@
 /* LAÇO DE REPETIÇÃO FOR
 Laços oferecem um jeito fácil e rápido de executar uma ação repetidas vezes. */
 
-for (let i=0; i<101; i++) {
+for (let i = 0; i < 101; i++) {
     document.getElementById("teste").innerHTML += i + " , ";
+}
+
+for(let i = 2023; i > 1900; i--){
+    document.getElementById("ano").innerHTML += "<option value='" + i + "'>"+ i + "</option>"
 }
