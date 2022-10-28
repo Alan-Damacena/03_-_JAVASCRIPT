8a91ad5a09311ccb79acca2acf9c00716232458e
Alan Damacena <alanfmdamacena@outlook.com>
2022-10-28 10:56:38 -0300

    js

--- 18_-_EVENTOS/01_-_ONCLICK/script.js
+++ 18_-_EVENTOS/01_-_ONCLICK/script.js
@@ -6,31 +6,13 @@ Eventos são acçõs disparadas pela interação dos usuários na página.
 
 Existem muitos eventos. Veja os mais utilizados:
 
-ONCLICK -> Disparado quando recebe um click.
-ONDBLCLICK -> Disparado quando clique duplo.
-ONMOUSEOVER -> Disparado quando o mouse está sobre.
-ONMOUSEOUT -> Disparado quando o mouse é movido para fora do elemento.
-ONMOUSEMOVE -> Disparado quando o mouse é movido no elemento.
-ONMOUSEDOWN -> Disparado quando o clique do botão foi pressionado.
-ONMOUSEUP -> Disparado quando o clique do botão é liberado.
-ONFOCUS -> Disparado quando o elemento recebe o foco. 
-ONCHANGE -> Disparado quando existe uma mudança no conteúdo.
-ONBLUR -> Disparado quando o elemento perde o foco.
-ONKEYDOWN -> Disparado quando uma tecla é pressionada.
-ONKEYPRESS -> Disparado quando uma tecla é pressionada e solta.
-ONKEYUP -> Disparado quando uma tecla é solta sobre um elemento.
-ONLOAD -> Disparado quando a página terminou de ser carregada. 
-ONRESIZE -> Disparado quando há um redimencionamento da janela. 
-
-*/
+ONCLICK -> Disparado quando recebe um click. */
 
 function evento_Click(){
-
     alert("ACIONANDO O EVENTO ONCLICK");
 
     document.body.style.backgroundColor = "orange";
     natura.style.display = "block";
-    natura.style.margin = "auto";
-    
+    natura.style.margin = "auto";   
 }
 
