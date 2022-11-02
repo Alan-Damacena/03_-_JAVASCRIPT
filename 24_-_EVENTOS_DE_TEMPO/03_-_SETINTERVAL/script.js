0452d421070154a08a238744607c413a0bd2bfef
Alan Damacena <alanfmdamacena@outlook.com>
2022-11-02 15:21:08 -0300

    js

---
+++ 24_-_EVENTOS_DE_TEMPO/03_-_SETINTERVAL/script.js
@@ -0,0 +1,19 @@
+/* EVENTOS DE TEMPO COM JAVASCRIPT
+
+Os eventos de tempo permitem a execução do código em intervalos de tempo especificados. Esses intervalos de tempo são chamados de eventos de cronometragem.
+
+Os dois métodos-chave para usar com Javascript são:
+
+setTimeout(function, tempo em milisegundos)
+-> Executa uma função, depois de esperar um número especificado de milissegundos.
+
+setInterval(function, milliseconds)
+-> É o mesmo que setTimeout(), mas repete a execução da função continuamente. */
+
+function ativarContagem(){
+    setInterval(function(){
+        let cronometro = document.getElementById("tempo").innerHTML;
+        let soma = parseInt(cronometro) + 1;
+        document.getElementById("tempo").innerHTML = soma;
+    }, 1000);
+}
