e5950adedfef1662d1f7eea8c2381bc5f9f13e3d
Alan Damacena <alanfmdamacena@outlook.com>
2022-11-04 11:54:04 -0300

    js

--- 18_-_EVENTOS/04_-_ONMOUSEOUT/script.js
+++ 18_-_EVENTOS/04_-_ONMOUSEOUT/script.js
@@ -1,38 +1,12 @@
-/* EVENTOS
+/* Eventos são acçõs disparadas pela interação dos usuários na página.
 
-Eventos são acçõs disparadas pela interação dos usuários na página.
+É o correto manejo desses eventos que tornam as páginas interativas e dinâmicas. Existem muitos eventos. Veja os mais utilizados:
 
-É o correto manejo desses eventos que tornam as páginas interativas e dinâmicas.
-
-Existem muitos eventos. Veja os mais utilizados:
-
-ONCLICK -> Disparado quando recebe um click.
-ONDBLCLICK -> Disparado quando clique duplo.
-ONMOUSEOVER -> Disparado quando o mouse está sobre.
-ONMOUSEOUT -> Disparado quando o mouse é movido para fora do elemento.
-ONMOUSEMOVE -> Disparado quando o mouse é movido no elemento.
-ONMOUSEDOWN -> Disparado quando o clique do botão foi pressionado.
-ONMOUSEUP -> Disparado quando o clique do botão é liberado.
-ONFOCUS -> Disparado quando o elemento recebe o foco. 
-ONCHANGE -> Disparado quando existe uma mudança no conteúdo.
-ONBLUR -> Disparado quando o elemento perde o foco.
-ONKEYDOWN -> Disparado quando uma tecla é pressionada.
-ONKEYPRESS -> Disparado quando uma tecla é pressionada e solta.
-ONKEYUP -> Disparado quando uma tecla é solta sobre um elemento.
-ONLOAD -> Disparado quando a página terminou de ser carregada. 
-ONRESIZE -> Disparado quando há um redimencionamento da janela. 
-
-*/
+ONMOUSEOUT -> Disparado quando o mouse é movido para fora do elemento. */
 
 function vira_verde(){
-
     let div = document.getElementById("teste");
-
     div.style.backgroundColor = "gray";
-
     volks.style.display = "block";
-
-    volks.style.margin = "auto";
-    
+    volks.style.margin = "auto";   
 }
-
--- 24_-_EVENTOS_DE_TEMPO/03_-_SETINTERVAL/script.js
+++ 24_-_EVENTOS_DE_TEMPO/03_-_SETINTERVAL/script.js
@@ -10,6 +10,7 @@ setTimeout(function, tempo em milisegundos)
 setInterval(function, milliseconds)
 -> É o mesmo que setTimeout(), mas repete a execução da função continuamente. */
 
+//EXERCICIO 01
 function ativarContagem(){
     tempo = setInterval(function(){
         let cronometro = document.getElementById("tempo").innerHTML;
@@ -19,5 +20,24 @@ function ativarContagem(){
 }
 
 function pararContagem(){
+   clearInterval(tempo)
+}
+
+//EXERCICIO 02
+function ativarContagem1(){
+    tempo = setInterval(function(){
+        let cronometro = document.getElementById("tempo1").innerHTML;
+        let soma = parseInt(cronometro) - 1;
+
+        if(soma === 0){
+            document.getElementById("tempo1").innerHTML = "TEMPO ESGOTADO!!!";
+            pararContagem1()
+        }else{
+            document.getElementById("tempo1").innerHTML = soma;
+        }
+    }, 1000);
+}
+
+function pararContagem1(){
    clearInterval(tempo)
 }
