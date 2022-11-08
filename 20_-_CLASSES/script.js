f3909581b68600d6d15d46e89955ad888b8fdfdf
Alan Damacena <alanfmdamacena@outlook.com>
2022-11-08 12:06:13 -0300

    js

--- 20_-_CLASSES/script.js
+++ 20_-_CLASSES/script.js
@@ -12,11 +12,17 @@ Assim como uma fábrica da vida real precisa das máquinas para construir os obj
 class Carro{
     constructor(valor1,valor2,valor3){
         this.marca = valor1;
-        this.modelo - valor2;
+        this.modelo = valor2;
         this.ano = valor3;
     }
+    buzina(){
+        return this.modelo + " BUZINOU: Biiiiiiiiiiiii";}
 }
 
 //CHAMA A CONSTRUÇÃO ATRÁVES DA FÁBRICA
-const uno = new Carro("Fiat", "Uno", 2001)
-console.log(uno)
+let uno = new Carro("Fiat", "Uno", 2001);
+let gol = new Carro("Volkswagen", "Gol", 2004);
+console.log(uno.buzina());
+console.log(gol);
+gol.ano = 2014;
+console.log(gol);
