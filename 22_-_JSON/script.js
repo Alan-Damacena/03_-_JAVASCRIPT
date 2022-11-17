94845cdb6f8df3d3e16a7dd7b85c4497bb49738e
Alan Damacena <alanfmdamacena@outlook.com>
2022-11-17 10:30:47 -0300

    js

--- 22_-_JSON/script.js
+++ 22_-_JSON/script.js
@@ -39,9 +39,16 @@ function buscarCEP(){
     ajax.send();
 
     ajax.onload = function(){
-        document.getElementById("texto").innerHTML = this.responseText;
+        
+        //TRANSFORMEI O TEXTO EM OBJETO
         let obj = JSON.parse(this.responseText);
-        alert("DDD: " + obj.ddd)
+
+        //AQUI PEGUEI OS VALORES QUE EU QUERIA
+        let logradouro = obj.logradouro;
+        let cidade = obj.localidade;
+        let estado = obj.uf;
+
+        document.getElementById("texto").innerHTML = "Logradouro: " + logradouro + "<br> Cidade: " + cidade + "<br> Estado: " + estado;
     }
 
 }
