e1df04e3b15299f76d667e8a7aee74b0f8df3e3c
Alan Damacena <alanfmdamacena@outlook.com>
2022-11-18 11:45:45 -0300

    js

--- 14_-_ARRAYS/script.js
+++ 14_-_ARRAYS/script.js
@@ -1,8 +1,4 @@
-/*
-
-ARRAYS
-
-Os arrays JavaScript são usados para armazenar vários valores em uma única variável. Diferente dos objetos, que funcionam com propriedades ou "nomes" que você dá para os itens dentro dele, os arrays não possuem propriedades. O item dentro dele é encontrado pela posição.
+/* Os arrays JavaScript são usados para armazenar vários valores em uma única variável. Diferente dos objetos, que funcionam com propriedades ou "nomes" que você dá para os itens dentro dele, os arrays não possuem propriedades. O item dentro dele é encontrado pela posição.
 
 Imagine um array como uma lista de itens, com controle, por ordem de posição dentro dele.
 
@@ -11,11 +7,10 @@ Ex: const lista = ["arroz","feijão","macarrão","leite"];
 A lista[0] (lista na posição 0)
 A lista[1] (lista na posição 1)
 
-E assim por diante.
+E assim por diante. */
 
-*/
                  //0      //1         //2       //3
-const lista = ["arroz", "feijão", "macarrão", "leite"];
+const lista = [" arroz", " feijão", " macarrão", " leite"];
 
 //Mudando item de uma array
 lista[0] = "café"
@@ -32,15 +27,11 @@ alert("O item 3 da lista é " + x);
 alert("O item 0 da lista é " + lista[0]);
 alert("A minha lista possui um total de " + lista.length + " itens!!!");
 alert("O último item da lista é " + lista[lista.length -1]);
-alert("Adicionado na lista o item " + lista.push("ALAN") + " , logo, o último item da lista é " + lista[lista.length -1])
+alert("Adicionado na lista o item " + lista.push(" ALAN") + " , logo, o último item da lista é " + lista[lista.length -1])
 
 console.log(lista);
+document.getElementById("lista").innerHTML = lista
 
+/*  FUNCIONA DESSE JEITO TAMBÉM
 
-/* 
-
-FUNCIONA DESSE JEITO TAMBÉM
-
-const lista = new Array("arroz", "feijão", "macarrão", "leite")
-
-*/
+const lista = new Array("arroz", "feijão", "macarrão", "leite") */
