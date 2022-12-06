962660147311a01bb4175a603e9e7798317c5363
Alan Damacena <alanfmdamacena@outlook.com>
2022-12-06 11:30:34 -0300

    js

--- 15_-_METODOS_PARA_ARRAYS/11_-_MAIOR_E_MENOR_NUMERO/script.js
+++ 15_-_METODOS_PARA_ARRAYS/11_-_MAIOR_E_MENOR_NUMERO/script.js
@@ -1,20 +1,3 @@
-/*
-
-ARRAYS
-
-Os arrays JavaScript são usados para armazenar vários valores em uma única variável. Diferente dos objetos, que funcionam com propriedades ou "nomes" que você dá para os itens dentro dele, os arrays não possuem propriedades. O item dentro dele é encontrado pela posição.
-
-Imagine um array como uma lista de itens, com controle, por ordem de posição dentro dele.
-
-Ex: const lista = ["arroz","feijão","macarrão","leite"];
-
-A lista[0] (lista na posição 0)
-A lista[1] (lista na posição 1)
-
-E assim por diante.
-
-*/
-                
 const lista = [ 40, 100, 19, 5, 49, 32, 89];
 
 function MaiorNumero(array){
@@ -27,6 +10,4 @@ function MenorNumero(array){
 
 document.getElementById("teste").innerHTML = MaiorNumero(lista);
 
-
 document.getElementById("teste2").innerHTML = MenorNumero(lista);
-
